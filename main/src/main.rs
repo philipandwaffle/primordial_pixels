9fc39ce6da630a9f80e68eba641bfe1b48671066
Philip Cox <philipcox197@gmail.com>
2026-03-17 10:58:23 +0000

    clean

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,5 +1,3 @@
-use std::time::Duration;
-
 use avian2d::prelude::*;
 use bevy::{
     prelude::*,
@@ -22,7 +20,6 @@ mod physics_lock;
 // mod runner;
 // mod sandbox;
 mod save;
-// mod spatial;
 mod util;
 mod world;
 
@@ -58,11 +55,6 @@ fn main() {
         PhysicsLockPlugin,
         WorldPlugin,
     ));
-    // .add_plugins(
-    //     AutomaticUpdate::<SpatialTracked>::new()
-    //         .with_frequency(Duration::from_secs_f32(0.3))
-    //         .with_transform(TransformMode::GlobalTransform),
-    // );
 
     a.run();
 }
